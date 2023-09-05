e14d73902cbbe6e7e090a592b6fe3827a99144c1
Clem <clemmartinez33240@gmail.com>
2023-09-05 07:54:30 +0200

    create NoResults component + connect product to store

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,17 +1,23 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 
-import Billboard from "@/components/ui/billboard";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container"
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
     const billboard = await getBillboard("7eca4609-57df-4b75-9823-1d3bf460e8c7");
 
     return (
         <Container>
             <div className="space-y-10 pb-10">
                 <Billboard data={billboard} />
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
             </div>
         </Container>
     )
--- components/billboard.tsx
+++ components/billboard.tsx
@@ -1,13 +1,18 @@
 import { Billboard } from "@/types";
 
+import NoResults from "@/components/ui/no-results";
+
 interface BillboardProps {
     data: Billboard
 };
 
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
+
     if (!data) {
-        return <p>No data available</p>
+        return (<NoResults />)
     }
+
+
 
     return (
         <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
@@ -15,7 +20,7 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
                 className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover "
                 style={{ backgroundImage: `url(${data?.imageUrl})` }}
             >
-                <div className="h-full w-full flex flex-col justify-center item-center text-center gap-y-8">
+                <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
                     <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
                         {data.label}
                     </div>
