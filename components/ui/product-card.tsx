25c9d8a358c1bc50a6a67dd831c03bb4dd5e2cb6
Clem <clemmartinez33240@gmail.com>
2023-09-07 10:02:54 +0200

    developping productTab section

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import { Expand } from "lucide-react";
+import { Expand, ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 import { Product } from "@/types";
 
 import IconButton from "@/components/ui/icon-button";
+import Currency from "@/components/ui/currency";
 
 
 interface ProductCard {
@@ -13,8 +15,14 @@ interface ProductCard {
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(`/product/${data?.id}`);
+    }
+
     return (
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             <div className="aspect-square rounded-xl bg-gray-100 relative">
                 <Image
                     src={data?.images?.[0].url}
@@ -28,8 +36,23 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
                             onClick={() => { }}
                             icon={<Expand size={20} className="text-gray-600" />}
                         />
+                        <IconButton
+                            onClick={() => { }}
+                            icon={<ShoppingCart size={20} className="text-gray-600" />}
+                        />
                     </div>
                 </div>
+            </div>
+            <div>
+                <p className="font-semibold text-lg">
+                    {data.name}
+                </p>
+                <p className="text-sm text-gray-500">
+                    {data.category?.name}
+                </p>
+            </div>
+            <div className="flex items-center justify-between">
+                <Currency value={data?.price} />
             </div>
         </div>
     )
